5d42ff128629ca0d70e4503c17af4a3b6ba44b84
Daurko <payton.saunders@students.mq.edu.au>
2025-09-16 12:59:10 +1000

    finished player forward and back movement

--- Week8COMP2160/Assets/Scripts/PlayerMovement.cs
+++ Week8COMP2160/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] public float moveSpeed = 5f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move(){
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection  = transform.right * moveX + transform.forward * moveZ;
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
